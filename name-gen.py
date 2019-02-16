8a3522b73d6ffbd18eac1a1f6d034db1b4cd7dd0
Micah Smith <smithml@gmail.com>
2019-02-16 16:05:47 -0600

    Create name-gen.py

---
+++ name-gen.py
@@ -0,0 +1,14 @@
+#Random name generator
+
+print('Number of characters:' ) #get number of characters
+numChars = input()
+
+#load character library - consonant table, vowel table, maybe more?
+
+#until numChars is reached
+
+    #select consonant
+
+    #select vowel
+
+#print name
