a5c7461ae29a943635db2ac000d489b0fbd5f1c3
Micah Smith <smithml@gmail.com>
2019-02-16 22:48:49 -0600

    Update name-gen.py

--- name-gen.py
+++ name-gen.py
@@ -1,14 +1,18 @@
 #Random name generator
+import random
 
-print('Number of characters:' ) #get number of characters
-numChars = input()
+print('Number of characters: ') #get number of characters
+charsMax = input()
+print('How many names: ') #number of names to generate
+qnames = input()
 
 #load character library - consonant table, vowel table, maybe more?
 
-#until numChars is reached
-
-    #select consonant
-
-    #select vowel
+#Until charsMax is reached, generate new names
+names = 0
+while names < qnames:
+    namesToOutput[names] = nameGen(cTab, vTab, charsMax)
 
 #print name
+for i in range(qnames):
+    print(namesToOutput[i]
